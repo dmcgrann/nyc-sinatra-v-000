ff3f97e1aa9fab5b60e8399b839329537d9bf64b
David McGrann <dpmcgrann@gmail.com>
2018-09-12 19:57:21 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -13,11 +13,16 @@ class LandmarksController < ApplicationController
    
   get '/landmark/:id' do
     @landmark = Landmark.find(params[:id])
+<<<<<<< HEAD
     erb :'landmarks/show'
+=======
+    erb :'landmark/show'
+>>>>>>> 9e655273c22ab86282dfe3b5d7c3836d1864c077
   end
   
   get '/landmark/:id/edit' do
     @landmark = Landmark.find(params[:id])
+<<<<<<< HEAD
     erb :'landmarks/edit'
   end
   
@@ -32,6 +37,22 @@ class LandmarksController < ApplicationController
     @landmark.update(params[:landmark])
     @landmark.save
     redirect to "/landmarks/#{@landmark.id}"
+=======
+    erb :'landmark/edit'
+  end
+  
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.name = params['landmark']['name']
+    @landmark.year_completed = params['landmark']['year_completed']
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+  
+  post '/landmarks' do
+    Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
+    redirect '/landmarks'
+>>>>>>> 9e655273c22ab86282dfe3b5d7c3836d1864c077
   end
   
  
